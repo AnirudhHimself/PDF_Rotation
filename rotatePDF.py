ec68a725f89f8a41ef920abafb521d42fa9eebb8
Anirudh Mylavarapu <anirudhhimself@gmail.com>
2018-12-16 17:16:32 -0600

    Added method for counterclockwise rotation
    Rotates all pages within the supplied PDF counterclockwise by 90 degrees

--- rotatePDF.py
+++ rotatePDF.py
@@ -1,5 +1,21 @@
 import PyPDF2
 
+def rotateCounterClockwise(original, new):
+	pdfFileObj = open(original, 'rb')
+	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+	pdfWriter = PyPDF2.PdfFileWriter()
+
+	for page in range(pdfReader.numPages):
+		pageObj = pdfReader.getPage(page)
+		pageObj.rotateCounterClockwise(90)
+		pdfWriter.addPage(pageObj)
+
+	newFile = open(new, 'wb')
+
+	pdfWriter.write(newFile)
+	pdfFileObj.close()
+	newFile.close()
+
 
 def rotateClockwise(original, new):
 	pdfFileObj = open(original, 'rb')
