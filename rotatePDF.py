963976fa8fd77db4fa0fd322181459b85a3f2805
Anirudh Mylavarapu <anirudhhimself@gmail.com>
2018-12-16 18:17:14 -0600

    Added a main method so that it works

--- rotatePDF.py
+++ rotatePDF.py
@@ -1,4 +1,15 @@
 import PyPDF2
+import sys
+
+def main():
+	if(len(sys.argv) < 4):
+		print("Usage: $ python rotatePdf.py direction fileToRotate rotatedFile")
+	elif(str(sys.argv[1]) == "clockwise"):
+		rotateClockwise(str(sys.argv[2]), str(sys.argv[3]))
+	elif(str(sys.argv[1]) == "counter"):
+		rotateCounterClockwise(str(sys.argv[2]), str(sys.argv[3]))
+	else:
+		flip(str(sys.argv[2]), str(sys.argv[3]))
 
 def flip(original, new):
 	pdfFileObj = open(original, 'rb')
@@ -16,7 +27,6 @@ def flip(original, new):
 	pdfFileObj.close()
 	newFile.close()
 
-
 def rotateCounterClockwise(original, new):
 	pdfFileObj = open(original, 'rb')
 	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
@@ -32,7 +42,6 @@ def rotateCounterClockwise(original, new):
 	pdfWriter.write(newFile)
 	pdfFileObj.close()
 	newFile.close()
-
 
 def rotateClockwise(original, new):
 	pdfFileObj = open(original, 'rb')
@@ -50,4 +59,5 @@ def rotateClockwise(original, new):
 	pdfFileObj.close()
 	newFile.close()
 
-
+if __name__ == '__main__':
+	main()
