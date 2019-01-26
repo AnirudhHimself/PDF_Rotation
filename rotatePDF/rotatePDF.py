f5c34556d6d0ec94be8567766030ee2f6f044b00
Anirudh Mylavarapu <anirudhhimself@gmail.com>
2019-01-26 13:30:31 -0600

    Reduced the number of functions

--- rotatePDF/rotatePDF.py
+++ rotatePDF/rotatePDF.py
@@ -4,53 +4,28 @@ import sys
 def main():
 	if(len(sys.argv) < 3):
 		print("Usage: $ python rotatePdf.py direction fileToRotate")
+		print("-- Options for direction --")
+		print("clockwise")
+		print("counter")
+		print("flip")
 	elif(str(sys.argv[1]) == "clockwise"):
-		rotateClockwise(str(sys.argv[2]), str(sys.argv[2])[:-3]+"Rotated.pdf")
+		rotate(str(sys.argv[2]), str(sys.argv[2])[:-3]+"Rotated.pdf", 90)
 	elif(str(sys.argv[1]) == "counter"):
-		rotateCounterClockwise(str(sys.argv[2]), str(sys.argv[2])[:-3]+"Rotated.pdf")
+		rotate(str(sys.argv[2]), str(sys.argv[2])[:-3]+"Rotated.pdf", 270)
 	else:
-		flip(str(sys.argv[2]), str(sys.argv[2])[:-3]+"Rotated.pdf")
+		rotate(str(sys.argv[2]), str(sys.argv[2])[:-3]+"Rotated.pdf", 180)
 
-def flip(original, new):
+
+
+
+def rotate(original, new, direction):
 	pdfFileObj = open(original, 'rb')
 	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 	pdfWriter = PyPDF2.PdfFileWriter()
 
 	for page in range(pdfReader.numPages):
 		pageObj = pdfReader.getPage(page)
-		pageObj.rotateClockwise(180)
-		pdfWriter.addPage(pageObj)
-
-	newFile = open(new, 'wb')
-
-	pdfWriter.write(newFile)
-	pdfFileObj.close()
-	newFile.close()
-
-def rotateCounterClockwise(original, new):
-	pdfFileObj = open(original, 'rb')
-	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-	pdfWriter = PyPDF2.PdfFileWriter()
-
-	for page in range(pdfReader.numPages):
-		pageObj = pdfReader.getPage(page)
-		pageObj.rotateCounterClockwise(90)
-		pdfWriter.addPage(pageObj)
-
-	newFile = open(new, 'wb')
-
-	pdfWriter.write(newFile)
-	pdfFileObj.close()
-	newFile.close()
-
-def rotateClockwise(original, new):
-	pdfFileObj = open(original, 'rb')
-	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-	pdfWriter = PyPDF2.PdfFileWriter()
-
-	for page in range(pdfReader.numPages):
-		pageObj = pdfReader.getPage(page)
-		pageObj.rotateClockwise(90)
+		pageObj.rotateClockwise(direction)
 		pdfWriter.addPage(pageObj)
 
 	newFile = open(new, 'wb')
